88655a7412c2508a1f45939c1a1c2b97d860a001
Kirill Simonov <kirillsimonov@gmail.com>
2020-05-30 21:11:28 +0300

    Добавил файл yellow_smile.py

---
+++ lab3/yellow_smile.py
@@ -0,0 +1,36 @@
+from graph import *
+
+#Rectangle
+penColor(0, 0, 0)
+penSize(2)
+brushColor("black")
+rectangle(130, 180, 260, 200)
+
+
+#Circles
+penColor(0, 0, 0)
+penSize(2)
+brushColor(255, 255, 0)
+circle(200, 150, 100)
+
+penColor(0, 0, 0)
+penSize(2)
+brushColor(255, 0, 0)
+circle(150, 110, 20)
+
+penColor(0, 0, 0)
+penSize(2)
+brushColor(0, 0, 0)
+circle(150, 110, 7)
+
+penColor(0, 0, 0)
+penSize(2)
+brushColor(255, 0, 0)
+circle(240, 110, 25)
+
+penColor(0, 0, 0)
+penSize(2)
+brushColor(0, 0, 0)
+circle(240, 110, 10)
+
+run()
